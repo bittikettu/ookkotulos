24e567bcd07219b8dec1345fbe2ee192c1b7881a
Juha <viidakkovekara@gmail.com>
2021-01-23 00:41:34 +0200

    urls

--- modeltest2/urls.py
+++ modeltest2/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+    path('events/', views.events, name='events'),
     path('login/',
          LoginView.as_view
          (
